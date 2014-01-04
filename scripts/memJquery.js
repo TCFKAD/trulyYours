22dd1737bb95d69ce591a0a0e63f6af9e6f47c53
Steeve <steeve.delius@gmail.com>
2014-01-03 22:33:48 -0800

    Basic Letter Feed functionality completed
    Got the JS code to add letters to feed working.  I may change or improve
    it later.

--- scripts/memJquery.js
+++ scripts/memJquery.js
@@ -1,21 +1,21 @@
 // JavaScript Document
-var letterbox = new Array();
-var loading = true;
-//alert("The spcript works!");
-var loadBox = $(function(){
-	var $letterL = $("<div class=\"leftLetter\"> </div>");	
-	var $letterR = $("<div class=\"rightLetter\"> </div>");	
-	letterBox.push($letterL);
-	letterBox.push($letterR);
-	alert(letterBox.length);
-});
-	
-var addToPage = $(function( letters ){
-	do{
-		$(letterFed).append(letters.pop());	
-	}while ((letters.length != 0)&&(loading))
-});
+$( document ).ready(function() {
+	letterBox = new Array();
+	loading = true
 
-alert("The spcript works!");
-loadBox();
-addToPage(letterBox);
+	var loadBox = function( letterBox ){
+		var $letterL = $("<div class=\"rightLetter\"> </div>");	
+		var $letterR = $("<div class=\"leftLetter\"> </div>");	
+		letterBox.push($letterL);
+		letterBox.push($letterR);
+	};
+		
+	var addToPage = function( letters ){
+		do{
+			$(letterFed).append(letters.pop());	
+		}while ((letters.length != 0)&&(loading))
+	};
+	
+	loadBox(letterBox);
+	addToPage(letterBox);
+});
